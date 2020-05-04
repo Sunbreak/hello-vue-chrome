935f370a5f8642ab885fd9427858b68d95b944ae
wangkun <wangkun@woodemi.com>
2020-05-04 12:04:30 +0800

    Use singleton Tab instead of popup

--- src/background.js
+++ src/background.js
@@ -1,4 +1,21 @@
-import store from './store';
 global.browser = require('webextension-polyfill');
 
-alert(`Hello ${store.getters.foo}!`);
+let singletonTabId;
+
+browser.runtime.onInstalled.addListener(function() {
+  browser.browserAction.onClicked.addListener(async function(currentTab) {
+    if (!singletonTabId) {
+      const url = browser.runtime.getURL('popup/popup.html');
+      const tab = await browser.tabs.create({ url });
+      singletonTabId = tab.id;
+      return;
+    }
+
+    const singletonTab = await browser.tabs.get(singletonTabId);
+    const currentWindow = await browser.windows.getCurrent();
+    if (currentWindow.id !== singletonTab.windowId) {
+      await browser.windows.update(singletonTab.windowId, { focused: true });
+    }
+    await browser.tabs.update(singletonTab.id, { active: true });
+  });
+});
